56844382eef15c413dce1c1a63e8d97c4f48cb3d
sdntechforum <63686526+sdntechforum@users.noreply.github.com>
2022-03-23 18:26:39 -0400

    Update led_blink.py

--- led_blink.py
+++ led_blink.py
@@ -1,1 +1,17 @@
+import RPi.GPIO as GPIO
+import time
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+led=21
+
+GPIO.setup(led,GPIO.OUT)
+
+try:
+	while True:
+    	  GPIO.output(led,True)
+    	  time.sleep(1)
+    	  GPIO.output(led,False)
+    	  time.sleep(1)
+finally:
+   GPIO.cleanup()
